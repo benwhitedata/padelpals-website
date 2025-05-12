cbc8e45baa3c9f484ed25b4172af299aa87b14cb
benwhite <benwhitedata@gmail.com>
2025-05-12 14:04:30 +0100

    Remove sensitive credentials from template file

--- config.template.js
+++ config.template.js
@@ -1,5 +1,5 @@
 // Supabase configuration template
 const config = {
-    supabaseUrl: 'https://peaphqbxdmknxzsfdxuh.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlYXBocWJ4ZG1rbnh6c2ZkeHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMTA5NzcsImV4cCI6MjA1NjU4Njk3N30.RYBFcTJC13TOajpWSCybcq-IExXNaEvfSPRkwG16Bt8'
+    supabaseUrl: 'YOUR_SUPABASE_URL',
+    supabaseKey: 'YOUR_SUPABASE_ANON_KEY'
 }; 
