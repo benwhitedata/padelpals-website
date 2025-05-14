09b88b3afb75ffafc62ccb182e0c0006011424df
benwhitedata <48286916+benwhitedata@users.noreply.github.com>
2025-05-14 08:31:23 +0000

    Deploying to gh-pages from @ benwhitedata/padelpals-website@6cf8fe6a42b1ccac5a92b8e93016bca5c306dc67 🚀

--- enhanced-config.js
+++ enhanced-config.js
@@ -1,5 +1,5 @@
 // Enhanced configuration file with fallback
-// Created by GitHub Actions at Wed May 14 08:23:48 UTC 2025
+// Created by GitHub Actions at Wed May 14 08:31:22 UTC 2025
 
 const config = {
     supabaseUrl: 'https://peaphqbxdmknxzsfdxuh.supabase.co',
